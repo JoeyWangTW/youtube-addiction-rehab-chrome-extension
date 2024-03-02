846c41da08ba20c05d5fd051b8025db5effa3da0
Joey Wang <joey840404@gmail.com>
2024-03-01 22:58:47 -0800

    Initial commit

---
+++ src/pages/options/Options.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '@pages/options/Options.css';
+
+const Options: React.FC = () => {
+  return <div className="container">Options</div>;
+};
+
+export default Options;
