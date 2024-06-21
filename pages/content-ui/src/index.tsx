0ca32015f86307a7b5980cccf088baa0b6877ef1
Joey Wang <joeywang@Joeys-MacBook-Pro.local>
2024-06-21 00:44:30 -0700

    content ui for title eval

--- pages/content-ui/src/index.tsx
+++ pages/content-ui/src/index.tsx
@@ -1,23 +1,49 @@
 import { createRoot } from 'react-dom/client';
-import App from '@src/app';
+import { TitleEvalResult } from '@src/app';
 // eslint-disable-next-line
 // @ts-ignore
 import tailwindcssOutput from '@src/tailwind-output.css?inline';
 
-const root = document.createElement('div');
-root.id = 'chrome-extension-boilerplate-react-vite-content-view-root';
+function addTitleEval(result) {
+  const existingRoot = document.getElementById('chrome-extension-boilerplate-react-vite-content-view-root');
 
-document.body.append(root);
+  if (existingRoot) {
+    // Update the existing component if it already exists
+    const shadowRoot = existingRoot.shadowRoot;
+    const contentComponent = shadowRoot.getElementById('shadow-root');
+    createRoot(contentComponent).render(<TitleEvalResult result={result} />);
+  } else {
+    // Create new component if it does not exist
+    const root = document.createElement('div');
+    root.id = 'chrome-extension-boilerplate-react-vite-content-view-root';
 
-const rootIntoShadow = document.createElement('div');
-rootIntoShadow.id = 'shadow-root';
+    document.querySelector('ytd-watch-metadata #title').append(root);
 
-const shadowRoot = root.attachShadow({ mode: 'open' });
-shadowRoot.appendChild(rootIntoShadow);
+    const rootIntoShadow = document.createElement('div');
+    rootIntoShadow.id = 'shadow-root';
 
-/** Inject styles into shadow dom */
-const styleElement = document.createElement('style');
-styleElement.innerHTML = tailwindcssOutput;
-shadowRoot.appendChild(styleElement);
+    const shadowRoot = root.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(rootIntoShadow);
 
-createRoot(rootIntoShadow).render(<App />);
+    /** Inject styles into shadow dom */
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = tailwindcssOutput;
+    shadowRoot.appendChild(styleElement);
+
+    createRoot(rootIntoShadow).render(<TitleEvalResult result={result} />);
+  }
+}
+
+document.addEventListener('yt-page-data-updated', async () => {
+  const metaDataElement = document.querySelector('ytd-watch-metadata');
+  if (metaDataElement) {
+    // Get the video title text
+    const videoTitle = metaDataElement.querySelector('yt-formatted-string')?.textContent;
+
+    console.log(videoTitle);
+    // Send the video title to the background script
+    const response = await chrome.runtime.sendMessage({ type: 'newVideoLoaded', videoTitle });
+    console.log(response);
+    addTitleEval(response);
+  }
+});
